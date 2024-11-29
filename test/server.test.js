4b1635a5d90fc23cc58e15e0ef3aec7a79c75b0d
pawanuk <pawanmantra@gmail.com>
2024-11-29 16:50:55 +0000

    new

--- test/server.test.js
+++ test/server.test.js
@@ -4,6 +4,7 @@ const app = require('../server');
 describe('GET /hello', () => {
   it('should return hello message', async () => {
     const response = await request(app).get('/hello');
+    
     expect(response.status).toBe(200);
     expect(response.body.message).toBe('Hello GitHub Actions!');
   });
